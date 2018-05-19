e7309c442bb07bfe0d728b48b774d2a60f4ebd50
Jonathan Maes <jonathan145b@hotmail.com>
2018-05-19 16:48:46 +0200

    Episode 4

--- Hardcore hermits/Hardcore hermits 2/run.bat
+++ Hardcore hermits/Hardcore hermits 2/run.bat
@@ -1,3 +1,9 @@
 @echo off
 python main.py
+echo Close the window now if you do not want to push to GitHub. Press enter to push anyway.
+pause >nul
+git status
+git add --all
+git commit -v
+git push -u origin master
 pause
